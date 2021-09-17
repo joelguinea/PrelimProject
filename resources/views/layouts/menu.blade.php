5bf722efd06bd7acb759d9e5e0cd3fa573ea076b
joel guinea <joelguinea69@gmail.com>
2021-09-17 15:14:35 +0800

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="{{ route('bookings.index') }}"
+       class="nav-link {{ Request::is('bookings*') ? 'active' : '' }}">
+        <p>Bookings</p>
+    </a>
+</li>
+
+
